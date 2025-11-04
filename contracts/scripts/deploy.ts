b6f91dd24b0bbe2b315969d49a85c93eb91ab9fe
AnjungL11 <lianjing52@gmail.com>
2025-11-04 20:29:52 +0800

    Update deployment script to include MyERC20 and OrderbookLib
    Deploys MyERC20 and OrderbookLib contracts before EasyBet.

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,11 +1,34 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const EasyBet = await ethers.getContractFactory("EasyBet");
-  const easyBet = await EasyBet.deploy();
+  console.log("Starting contract deployment...");
+  console.log("Deploying MyERC20...");
+  const MyERC20 = await ethers.getContractFactory("MyERC20");
+  const myERC20 = await MyERC20.deploy();
+  await myERC20.deployed();
+  console.log(`MyERC20 deployed to ${myERC20.address}`);
+
+  console.log("Deploying OrderbookLib...");
+  const OrderbookLib = await ethers.getContractFactory("OrderbookLib");
+  const orderbookLib = await OrderbookLib.deploy();
+  await orderbookLib.deployed();
+  console.log(`OrderbookLib deployed to ${orderbookLib.address}`);
+
+  console.log("Deploying EasyBet...");
+  // const EasyBet = await ethers.getContractFactory("EasyBet");
+  const EasyBet = await ethers.getContractFactory("EasyBet", {
+    libraries: {
+      "OrderbookLib": orderbookLib.address
+    }
+  });
+  const easyBet = await EasyBet.deploy(myERC20.address);
   await easyBet.deployed();
 
   console.log(`EasyBet deployed to ${easyBet.address}`);
+  console.log("Deployment completed!");
+  console.log("MyERC20 address:", myERC20.address);
+  console.log("OrderbookLib address:", orderbookLib.address);
+  console.log("EasyBet address:", easyBet.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
